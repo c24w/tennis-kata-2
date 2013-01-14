750424c874b63918473dcdbd9aa40661d6740d64
Chris Watson <chris.watson@7digital.com>
2013-01-14 17:12:53 +0000

    Add implementation for love - love

---
+++ TennisKata/TennisKataTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace TennisKata
+{
+	[TestFixture]
+	public class TennisKataTests
+	{
+		[Test]
+		public void Tennis_game_should_start_player_scores_on_zero()
+		{
+			var tennisGame = new TennisGame();
+
+			Assert.That(tennisGame.GetScore(), Is.EqualTo("love - love"));
+		}
+	}
+
+	public class TennisGame {
+		public string GetScore()
+		{
+			return "love - love";
+		}
+	}
+}
