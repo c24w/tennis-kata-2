ec19840f0ee9dcaf65e5e56f05a39e0b104ff984
Chris Watson <chris.watson@7digital.com>
2013-01-21 17:52:06 +0000

    Fixed logic for Deuce. Added tests for both players scoring the same amount before Deuce.

--- TennisKata/TennisKataTests.cs
+++ TennisKata/TennisKataTests.cs
@@ -51,12 +51,22 @@ namespace TennisKata
 
 		[TestCase(0, "Love")]
 		[TestCase(1, "Fifteen")]
+		[TestCase(2, "Thirty")]
 		public void Tennis_game_returns_the_expected_score_for_fifteen_all(int pointsEach, string expectedScore)
 		{
 			PlayerOneScoresTimes(pointsEach);
 			PlayerTwoScoresTimes(pointsEach);
 
 			Assert.That(_tennisGame.GetScore(), Is.EqualTo(expectedScore + " all"));
+		}
+
+		[Test]
+		public void Tennis_game_returns_the_expected_score_for_deuce()
+		{
+			PlayerOneScoresTimes(3);
+			PlayerTwoScoresTimes(3);
+
+			Assert.That(_tennisGame.GetScore(), Is.EqualTo("Deuce"));
 		}
 
 		private void PlayerOneScoresTimes(int times)
@@ -97,11 +107,22 @@ namespace TennisKata
 				return Scores.Game + " player 1";
 			if (_playerTwoScore == Scores.Game)
 				return Scores.Game + " player 2";
-			if (_playerOneScore == _playerTwoScore)
+			if (IsDeuce())
+				return "Deuce";
+			if (ScoresAreEqualBeforeDeuce())
 				return _playerOneScore + " all";
 			return _playerOneScore + Separator + _playerTwoScore;
 		}
 
+		bool IsDeuce()
+		{
+			return _playerOneScore == Scores.Forty && _playerTwoScore == Scores.Forty;
+		}
+
+		private bool ScoresAreEqualBeforeDeuce()
+		{
+			return _playerOneScore == _playerTwoScore && _playerOneScore != Scores.Forty;
+		}
 
 
 		public void PlayerOneScores()
